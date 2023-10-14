e5a6662f7114b274cfeda82e91245b5bb54a9e12
d-yx <352104389@qq.com>
2023-10-14 22:41:05 +0800

    feat: add loading page

---
+++ src/loading.jsx
@@ -0,0 +1,19 @@
+import { Spin } from 'antd';
+
+function loading() {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Spin size="large" />
+    </div>
+  );
+}
+
+export default loading;
